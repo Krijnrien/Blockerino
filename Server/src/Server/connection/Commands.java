e60c5142b287babe89efb9ede87691cd80044b16
Burg,Krijn K.R. van der <k.vanderburg@student.fontys.nl>
2017-11-30 00:05:45 +0100

    Rough beginning of object <> function caller hashmap implementation;

--- Server/src/Server/connection/Commands.java
+++ Server/src/Server/connection/Commands.java
@@ -1,16 +1,18 @@
 package Server.connection;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 
 public class Commands {
     //TODO Hashmap with functions equaling command
     //TODO Maybe place connectedUsers list here?
+
+    Map<Character, Runnable> clientCommands = new HashMap<>();
     List list = Collections.synchronizedList(new ArrayList());
 
-    private Commands() {}
+
+    private Commands() {
+    }
 
     private static class InstanceHolder {
         private static final Commands instance = new Commands();
